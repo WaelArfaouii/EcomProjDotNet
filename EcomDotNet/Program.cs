5fdcb28b64a3b2f82fab2dbdab8bec3184f127f7
Wael <waelarfaoui@ieee.org>
2023-03-21 11:08:32 +0100

    migrate to database

--- EcomDotNet/Program.cs
+++ EcomDotNet/Program.cs
@@ -4,53 +4,64 @@ using Infrastructure.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddScoped<IProductRepository,ProductRepository>();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(options =>
-    {
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-    });
-
-var app = builder.Build();
-using (var scope = app.Services.CreateScope()){
-
-    var services = scope.ServiceProvider ;
-    var loggerFactory = services.GetRequiredService<ILoggerFactory>() ; 
-    try {
-        var context = services.GetRequiredService<DataContext>() ;
-        await context.Database.MigrateAsync() ;
-    }
-    catch(Exception ex) {
-
-        var logger = loggerFactory.CreateLogger<Program>();
-        logger.LogError(ex,"An error occured during migration !") ;
-    }
-    
-    
-    
-    }
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddDbContext<DataContext>(options =>
+            {
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
+
+        var app = builder.Build();
+        using (var scope = app.Services.CreateScope())
+        {
+
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+                await DataContextSeed.SeedAsync(context, loggerFactory);
+            }
+            catch (Exception ex)
+            {
+
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "An error occured during migration !");
+            }
+
+
+
+        }
+        
+
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
